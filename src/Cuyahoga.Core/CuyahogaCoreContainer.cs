3570911d0dedd169410e21f978eab84d5644a2af
xcrash <grishinage@gmail.com>
2010-10-21 13:41:00 +0400

    extract CuyhogaCoreContaineer

---
+++ src/Cuyahoga.Core/CuyahogaCoreContainer.cs
@@ -0,0 +1,45 @@
+﻿using Castle.Windsor;
+using Castle.Windsor.Configuration.Interpreters;
+using Cuyahoga.Core.Service;
+
+namespace Cuyahoga.Core
+{
+	public class CuyahogaCoreContainer : WindsorContainer
+	{
+		/// <summary>
+		/// Constructor. The configuration is read from the web.config.
+		/// </summary>
+		public CuyahogaCoreContainer() : base(new XmlInterpreter())
+		{
+			Initialize();
+		}
+
+		private void Initialize()
+		{
+			RegisterFacilities();
+			RegisterServices();
+			ConfigureLegacySessionFactory();
+		}
+
+		protected virtual void RegisterFacilities()
+		{
+		}
+
+		protected virtual void RegisterServices()
+		{
+			// The core services are registrated via services.config
+
+			// Utility services
+			AddComponent("core.sessionfactoryhelper", typeof (SessionFactoryHelper));
+
+			// Legacy
+			AddComponent("corerepositoryadapter", typeof (CoreRepositoryAdapter));
+		}
+
+		protected virtual void ConfigureLegacySessionFactory()
+		{
+			var sessionFactoryHelper = Resolve<SessionFactoryHelper>();
+			sessionFactoryHelper.ConfigureLegacySessionFactory();
+		}
+	}
+}
--- src/Cuyahoga.Web/Components/CuyahogaContainer.cs
+++ src/Cuyahoga.Web/Components/CuyahogaContainer.cs
@@ -1,44 +1,23 @@
-using Castle.Windsor;
-using Castle.Windsor.Configuration.Interpreters;
-using Cuyahoga.Core.Service;
+using Cuyahoga.Core;
 
 namespace Cuyahoga.Web.Components
 {
 	/// <summary>
 	/// The CuyahogaContainer serves as the IoC container for Cuyahoga.
 	/// </summary>
-	public class CuyahogaContainer : WindsorContainer
+	public class CuyahogaContainer : CuyahogaCoreContainer
 	{
 		/// <summary>
 		/// Constructor. The configuration is read from the web.config.
 		/// </summary>
-		public CuyahogaContainer() : base(new XmlInterpreter())
-		{
-			RegisterFacilities();
-			RegisterServices();
-			ConfigureLegacySessionFactory();
-		}
-
-		private void RegisterFacilities()
+		public CuyahogaContainer() : base()
 		{
 		}
 
-		private void RegisterServices()
+		protected override void RegisterServices()
 		{
-			// The core services are registrated via services.config
-			
-			// Utility services
-			AddComponent("web.moduleloader", typeof(ModuleLoader));
-			AddComponent("core.sessionfactoryhelper", typeof(SessionFactoryHelper));
-
-			// Legacy
-			AddComponent("corerepositoryadapter", typeof(CoreRepositoryAdapter));
-		}
-
-		private void ConfigureLegacySessionFactory()
-		{
-			SessionFactoryHelper sessionFactoryHelper = this.Resolve<SessionFactoryHelper>();
-			sessionFactoryHelper.ConfigureLegacySessionFactory();
+			AddComponent("web.moduleloader", typeof (ModuleLoader));
+			base.RegisterServices();
 		}
 	}
 }
